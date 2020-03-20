987a3046067d7dc679fb16913956b3babe4a9a03
Coul <coul422@gmail.com>
2020-03-20 19:45:13 -0400

    Add variables and constructor to ship

--- src/main/java/Ship.java
+++ src/main/java/Ship.java
@@ -1,5 +1,12 @@
 public class Ship {
-    Ship(){
-
+    int length;
+    boolean horizontal = false; // when this is false the ship is vertical
+    Player player;
+    int location;
+    Ship(Player player, int length, int location){
+        this.player = player;
+        this.length = length;
+        this.location = location;
     }
+
 }
